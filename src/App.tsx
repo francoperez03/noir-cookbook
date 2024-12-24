f72fe0d7f141f87ccf2e55f6a4fb75b3e8079cd8
francoperez03 <franco.perez03@gmail.com>
2024-12-24 09:31:04 -0300

    Squashed commit of the following:
    commit 7ab015171b9562363274732b000acba33c654917
    Author: francoperez03 <franco.perez03@gmail.com>
    Date:   Tue Dec 24 09:30:44 2024 -0300
    
        chore: add docs/proof_creation_flow.md
    
    commit b56d2965b3eecc9f5a7f94b3cfecc76e4642bd00
    Author: francoperez03 <franco.perez03@gmail.com>
    Date:   Mon Dec 23 19:48:00 2024 -0300
    
        feat: new VerifyProof and VerifyInput

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import Hero from './components/Hero/Hero';
-import ProofForm from './components/ProofForm/ProofForm';
-import './App.css';
-import './components/Hero/Hero.css';
-import './components/ProofForm/ProofForm.css';
+import { useState } from "react";
+import Hero from "./components/Hero/Hero";
+import ProofForm from "./components/ProofForm/ProofForm";
+import VerifyProof from "./components/VerifyProof/VerifyProof";
+import "./App.css";
+import "./components/Hero/Hero.css";
+import "./components/ProofForm/ProofForm.css";
+import { ProofData } from "@aztec/bb.js";
+import ProofInput from "./components/ProofInput/ProofInput";
 
 function App() {
+  const [proof, setProof] = useState<ProofData | null>(null);
+
   return (
     <div className="app-container">
-      <Hero />
-      <ProofForm />
+      {/* Hero at the top */}
+      <div className="hero">
+        <Hero />
+      </div>
+
+      {/* Form and Input side by side */}
+      <div className="main-content">
+        <div className="left-section">
+          <ProofForm onProofGenerated={(newProof) => setProof(newProof)} />
+        </div>
+        <div className="right-section">
+          <ProofInput proof={proof} onProofChange={(newProof) => setProof(newProof)} />
+          <VerifyProof proof={proof} />
+        </div>
+      </div>
     </div>
   );
 }
